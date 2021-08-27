42bfa2fb0c3c5bd7280fac54c93bcf59133406b4
Rishikesh Shinde <rdshinde2000@gmail.com>
2021-08-27 13:32:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashReceived = document.querySelector("#cash-received");
+const checkBtn = document.querySelector("#check-btn");
+const cashInputDiv = document.querySelector(".cash-input");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const msgDiv = document.querySelector(".messages");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+// disabling check button until inputs are taken
+checkBtn.disabled = true;
+function stateHandle() {
+  if (billAmount.value === "" || cashReceived.value === "") {
+    checkBtn.disabled = true;
+    msgDiv.classList.add("hidden");
+  } else {
+    checkBtn.disabled = false;
+    msgDiv.classList.add("hidden");
+  }
+}
+// event listener for button state handler
+billAmount.addEventListener("change", stateHandle);
+cashReceived.addEventListener("change", stateHandle);
+
+// function to calculate change
+function changeCalculator(ammountToReturn) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(ammountToReturn / availableNotes[i]);
+    ammountToReturn = ammountToReturn % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+function message(msg) {
+  msgDiv.classList.remove("hidden");
+  msgDiv.innerText = msg;
+}
+function inputValidator() {
+  if (billAmount.value > 0) {
+    var ammountToReturn = Math.trunc(cashReceived.value - billAmount.value);
+    if (ammountToReturn < 0) {
+      message("Customer needs to pay atleast full bill amount.");
+    } else if (ammountToReturn > 0) {
+      changeCalculator(ammountToReturn);
+    } else {
+      message("Amount satisfied!");
+    }
+  }
+  else{
+      message("Invalid Bill Amount!")
+  }
+}
+
+
+checkBtn.addEventListener("click", inputValidator);
