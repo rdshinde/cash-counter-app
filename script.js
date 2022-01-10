407b1bd9cb1a561fd8c7a3065b884b9b968eb7e9
RISHIKESH DNYANDEV SHINDE <67017632+rdshinde@users.noreply.github.com>
2022-01-10 14:53:01 +0530

    Updated to ES6

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 // disabling check button until inputs are taken
 checkBtn.disabled = true;
-function stateHandle() {
+const stateHandle = () =>{
   if (billAmount.value === "" || cashReceived.value === "") {
     checkBtn.disabled = true;
     msgDiv.classList.add("hidden");
@@ -22,20 +22,20 @@ billAmount.addEventListener("change", stateHandle);
 cashReceived.addEventListener("input", stateHandle);
 
 // function to calculate change
-function changeCalculator(ammountToReturn) {
+const changeCalculator = ammountToReturn => {
   for (let i = 0; i < availableNotes.length; i++) {
     const numberOfNotes = Math.trunc(ammountToReturn / availableNotes[i]);
     ammountToReturn = ammountToReturn % availableNotes[i];
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
-function message(msg) {
+const message = msg => {
   msgDiv.classList.remove("hidden");
   msgDiv.innerText = msg;
 }
-function inputValidator() {
+const inputValidator = () => {
   if (billAmount.value > 0) {
-    var ammountToReturn = Math.trunc(cashReceived.value - billAmount.value);
+    const ammountToReturn = Math.trunc(cashReceived.value - billAmount.value);
     if (ammountToReturn < 0) {
       message("Customer needs to pay atleast full bill amount.");
     } else if (ammountToReturn > 0) {
